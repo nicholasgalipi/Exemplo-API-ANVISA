118f082e24c838100b4f0141d45cd3062515b43b
Nicholas Galipi Gomes <42624206+nicholasgalipi@users.noreply.github.com>
2021-07-22 18:14:54 -0300

    Added first basic router

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const data = require('./preco_remedio_consumidor.json');
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
+const logger = require('koa-logger');  
+
 
 const app = new Koa();
 const router = new KoaRouter({
     prefix: '/medicamento'
 });
 
+app.use(logger((str, args) => {
+    console.log(str, args);
+}))
 app.listen(8080);
 app.use(router.routes());
 
-//ctx.params.name
-router.get('/get_by_name/:name', async ctx => (ctx.body = ctx.params.name));
+
+ 
+
+//ROUTERS
+//router.get('/get_by_name/:name', async ctx => (ctx.body = ctx.params.name));
+router.get('/get_by_name/:name', async ctx => (await getByName(ctx)));
+
+
+
+
+
+//controllers
+function getByName(ctx){
+    
+    for (let index = 0; index < data.length; index++) {
+        if(ctx.params.name == data[index]['SUBSTÂNCIA']){
+            ctx.body = data[index];
+            break;
+        }
+        
+    }
+}
+
