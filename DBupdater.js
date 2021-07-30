fa3863edef5aef6dcd8a8395fadbea1af270c812
Nicholas Galipi Gomes <42624206+nicholasgalipi@users.noreply.github.com>
2021-07-30 15:01:06 -0300

    DB updater changes

--- DBupdater.js
+++ DBupdater.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const https = require('https');
 const csv = require('csvtojson');
-const csvFilePath='./newtable.csv';
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';  
 
 const url = 'https://dados.anvisa.gov.br/dados/TA_PRECO_MEDICAMENTO.csv';
@@ -14,26 +14,13 @@ https.get(url,(res) => {
         filePath.close();
         console.log('Download Completed'); 
     })
-}).then(
-    csv()
-    .fromFile(csvFilePath)
-    .then((jsonObj)=>{fs.writeFile('myjsonfile.json', JSON.stringify(jsonObj), 'utf8',function (err) {
-        if (err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
-  }
-
-  console.log("JSON file has been saved.");
-})})
-)
-
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';  
+})
 
 
 
 
 
-/*const csvFilePath='./newtable.csv';
+const csvFilePath='./newtable.csv';
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{fs.writeFile('myjsonfile.json', JSON.stringify(jsonObj), 'utf8',function (err) {
@@ -43,4 +30,7 @@ csv()
   }
 
   console.log("JSON file has been saved.");
-})})*/
+})})
+
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';  
