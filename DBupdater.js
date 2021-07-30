abc3c1cd00d64028252eb7ee52d4c35388dcff1f
Nicholas Galipi Gomes <42624206+nicholasgalipi@users.noreply.github.com>
2021-07-30 14:52:23 -0300

    updates to de DBupdater

--- DBupdater.js
+++ DBupdater.js
@@ -1,16 +1,46 @@
 const fs = require('fs');
 const https = require('https');
-  
-// URL of the image
+const csv = require('csvtojson');
+const csvFilePath='./newtable.csv';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';  
+
 const url = 'https://dados.anvisa.gov.br/dados/TA_PRECO_MEDICAMENTO.csv';
   
 https.get(url,(res) => {
-    // Image will be stored at this path
-    const path = './precos.csv'; 
+    const path = './newtable.csv'; 
     const filePath = fs.createWriteStream(path);
     res.pipe(filePath);
     filePath.on('finish',() => {
         filePath.close();
         console.log('Download Completed'); 
     })
-})
+}).then(
+    csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{fs.writeFile('myjsonfile.json', JSON.stringify(jsonObj), 'utf8',function (err) {
+        if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+  }
+
+  console.log("JSON file has been saved.");
+})})
+)
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';  
+
+
+
+
+
+/*const csvFilePath='./newtable.csv';
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{fs.writeFile('myjsonfile.json', JSON.stringify(jsonObj), 'utf8',function (err) {
+  if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+  }
+
+  console.log("JSON file has been saved.");
+})})*/
