06b18fbc85c245d883819c30f33b779c12aa3e9e
Nicholas Galipi Gomes <42624206+nicholasgalipi@users.noreply.github.com>
2021-08-03 17:55:13 -0300

    Controllers refactor
    Controllers now filter inputs and search will work better

--- Controllers.js
+++ Controllers.js
@@ -2,8 +2,46 @@ const data = require('./preco_remedio_consumidor.json');
 
 exports.add = (num1,num2) => {return num1+num2};
 
+function normalizer(string) {
+    return string.normalize('NFD').replace(/[\u0300-\u036f]|[\W]/g, '').trim();
+}
 
 exports.getByName = (ctx) => { //get pelo nome comercial
+    
+    const regex = new RegExp(normalizer(ctx.params.name),"gi");
+    //let filtered = data.filter( test => (normalizer(test['PRODUTO']).search(regex) >= 0));
+    let filtered = data.filter( test => (regex.test(normalizer(test['PRODUTO'])) == true));
+    
+    //response handling
+    if(filtered.length > 0){
+        ctx.body = filtered;
+    }else{
+        ctx.status = 404;
+        ctx.body = {message: "Bad request"};
+    }
+    
+}
+
+
+exports.getByActive = (ctx) => { //get pelo nome do principio ativo
+    
+    const regex = new RegExp(normalizer(ctx.params.name),"gi");
+    let filtered = data.filter( test => (regex.test(normalizer(test['SUBSTÂNCIA'])) == true));
+    
+    //response handling
+    if(filtered.length > 0){
+        ctx.body = filtered;
+    }else{
+        ctx.status = 404;
+        ctx.body = {message: "Bad request"};
+    }
+}
+
+
+
+
+
+/*exports.getByName = (ctx) => { //get pelo nome comercial
     //console.log(ctx);
     let filtered = data.filter( test => (ctx.params.name == test['PRODUTO']));
     
@@ -28,4 +66,4 @@ exports.getByActive = (ctx) => { //get pelo nome do principio ativo
         ctx.body = {message: "Not Found"};
     }
     
-}
+}*/
--- Controllers.test.js
+++ Controllers.test.js
@@ -4,3 +4,15 @@ const Controllers = require('./Controllers');
 test('add test', () => {
     expect(Controllers.add(4,2)).toBe(6);
   });
+
+
+
+
+/*import fetch from 'node-fetch'
+
+  describe("test requests", () => {
+      test("bad request", async () => {
+          const data = await fetch('http://localhost:8080/get_by_produto/!@#!@').then(res => res.json())
+          expect(data).toEqual({ error: 'Caracteres inválidos no parâmetro.' });
+      });
+  })*/
